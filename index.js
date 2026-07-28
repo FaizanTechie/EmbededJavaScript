478c5730f9b9c2d5d2ac4ee093da6abc21d04308
M Faizan ul Haq <faizanulhaq391@gmail.com>
2026-07-29 03:34:17 +0500

    Practiced EJS

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-// const path = require("path");
+const path = require("path");
 
 const port = 8080;
 
 app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
@@ -15,6 +15,22 @@ app.get("/hello", (req, res) => {
     res.send("Hello");
 });
 
+///////////////////////////////////////////////////////
+
+// Passing data to ejs.
+// Example Roll Dice.
+
+// app.get("/rolldice", (req, res) => {
+//     res.render("rolldice.ejs");
+// })
+
+// ⬇️ Like data accessing from database.
+ app.get("/rolldice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) +1;
+    res.render("rolldice.ejs", {num : diceVal}); // {diceVal}
+})
+
+///////////////////////////////////////////////////////
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
